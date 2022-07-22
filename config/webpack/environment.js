5d0aea2829b647dc9ef31d5e72846f52f129e7f4
boradami <ami.logicalstreet@gmail.com>
2022-07-22 16:47:02 +0530

    Fixed changes

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,13 @@
 const { environment } = require('@rails/webpacker')
 
+const customConfig = {
+  node: {
+    fs: 'empty',
+    module: 'empty',
+    net: 'empty'
+  }
+};
+
+environment.config.merge(customConfig);
+
 module.exports = environment
