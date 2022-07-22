966e347589aa371b2bf2404a2a392998ffdec34e
boradami <ami.logicalstreet@gmail.com>
2022-07-22 14:03:05 +0530

    Fixed changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_18_105515) do
+ActiveRecord::Schema.define(version: 2022_07_22_063549) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,9 +29,9 @@ ActiveRecord::Schema.define(version: 2022_07_18_105515) do
 
   create_table "bookings", force: :cascade do |t|
     t.bigint "show_id", null: false
-    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
     t.index ["show_id"], name: "index_bookings_on_show_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
@@ -100,16 +100,6 @@ ActiveRecord::Schema.define(version: 2022_07_18_105515) do
     t.index ["screen_id"], name: "index_shows_on_screen_id"
   end
 
-  create_table "sub_prices", force: :cascade do |t|
-    t.integer "price"
-    t.bigint "show_id", null: false
-    t.bigint "seat_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["seat_id"], name: "index_sub_prices_on_seat_id"
-    t.index ["show_id"], name: "index_sub_prices_on_show_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -129,6 +119,4 @@ ActiveRecord::Schema.define(version: 2022_07_18_105515) do
   add_foreign_key "shows", "cinemas"
   add_foreign_key "shows", "movies"
   add_foreign_key "shows", "screens"
-  add_foreign_key "sub_prices", "seats"
-  add_foreign_key "sub_prices", "shows"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,43 +8,43 @@
 
 
 
-cinema1 = Cinema.create(name: "City Gold", address: "Bapunagar, Ahmedabad")
-p cinema1
-cinema2 = Cinema.create(name: "Devi Multiplex", address: "Naroda, Ahmedabad")
-p cinema2
-cinema3 = Cinema.create(name: "Rajhans", address: "Nikol, Ahmedabad")
-p cinema3
+# cinema1 = Cinema.create!(name: "City Gold", address: "Bapunagar, Ahmedabad")
+# p cinema1
+# cinema2 = Cinema.create!(name: "Devi Multiplex", address: "Naroda, Ahmedabad")
+# p cinema2
+# cinema3 = Cinema.create!(name: "Rajhans", address: "Nikol, Ahmedabad")
+# p cinema3
 
-movie1 = Movie.create(name: "jugjugg jeeyo", description: "nice movie", duration: "2h 30m", language: "Hindi", movie_type: "Drama", release_date: "24/6/2022")
-p movie1
-movie2 = Movie.create(name: "Naadi Dosh", description: "nice movie", duration: "2h 11m",language: "Gujarati", movie_type: "Comedy/Romantic", release_date: "17/6/2022")
-p movie2
-movie3 = Movie.create(name: "RRR", description: "nice movie", duration: "2h 20m",language: "Hindi", movie_type: "Action/Drama", release_date: "13/3/2022")
-p movie3
+# movie1 = Movie.create!(name: "jugjugg jeeyo", description: "nice movie", duration: "2h 30m", language: "Hindi", movie_type: "Drama", release_date: "24/6/2022")
+# p movie1
+# movie2 = Movie.create!(name: "Naadi Dosh", description: "nice movie", duration: "2h 11m",language: "Gujarati", movie_type: "Comedy/Romantic", release_date: "17/6/2022")
+# p movie2
+# movie3 = Movie.create!(name: "RRR", description: "nice movie", duration: "2h 20m",language: "Hindi", movie_type: "Action/Drama", release_date: "13/3/2022")
+# p movie3
 
-screen1 = Screen.create(screen_name: "screen1", total_seats: 120, cinema_id: 3)
-p screen1
-screen2 = Screen.create(screen_name: "screen2", total_seats: 160, cinema_id: 1)
-p screen2
-screen3 = Screen.create(screen_name: "screen3", total_seats: 120, cinema_id: 2)
-p screen3
+# screen1 = Screen.create!(screen_name: "screen1", total_seats: 120, cinema_id: 3)
+# p screen1
+# screen2 = Screen.create!(screen_name: "screen2", total_seats: 160, cinema_id: 1)
+# p screen2
+# screen3 = Screen.create!(screen_name: "screen3", total_seats: 120, cinema_id: 2)
+# p screen3
 
 
-show1 = Show.create(show_time: "2022-06-30 09:59:33.701477526 +0530", amount: 100, screen_id: 1, movie_id: 1, cinema_id: 3)
-p show1
-show2 = Show.create(show_time: "2022-06-30 11:04:36.501762601 +0530", amount: 120, screen_id: 2, movie_id: 2, cinema_id: 3)
-p show2
-show3 = Show.create(show_time: "2022-06-30 09:59:33.701477526 +0530", amount: 100, screen_id: 3, movie_id: 3, cinema_id: 1)
-p show3
-show4 = Show.create(show_time: "2022-06-30 11:04:36.501762601 +0530", amount: 120, screen_id: 2, movie_id: 2, cinema_id: 1)
-p show4
+# show1 = Show.create!(show_start_time: "2022-06-30 09:59:33.701477526 +0530", show_end_time: "2022-06-30 11:59:33.701477526 +0530", amount: 100, screen_id: 1, movie_id: 1, cinema_id: 3)
+# p show1
+# show2 = Show.create!(show_start_time: "2022-06-30 11:04:36.501762601 +0530", show_end_time: "2022-06-30 13:04:36.501762601 +0530", amount: 120, screen_id: 2, movie_id: 2, cinema_id: 3)
+# p show2
+# show3 = Show.create!(show_start_time: "2022-06-30 09:59:33.701477526 +0530", show_end_time: "2022-06-30 11:59:33.701477526 +0530", amount: 100, screen_id: 3, movie_id: 3, cinema_id: 1)
+# p show3
+# show4 = Show.create!(show_start_time: "2022-06-30 11:04:36.501762601 +0530", show_end_time: "2022-06-30 13:04:36.501762601 +0530", amount: 120, screen_id: 2, movie_id: 2, cinema_id: 1)
+# p show4
 
-(3..120).each do |seat|
-  p seat = Seat.create!(seat_number: "A#{seat}", screen_id: 1)
-end
-(1..160).each do |seat|
-  p Seat.create!(seat_number: "B#{seat}", screen_id: 2)
-end
+# (3..120).each do |seat|
+#   p seat = Seat.create!(seat_number: "A#{seat}", screen_id: 1)
+# end
+# (1..160).each do |seat|
+#   p Seat.create!(seat_number: "B#{seat}", screen_id: 2)
+# end
 
 
 
